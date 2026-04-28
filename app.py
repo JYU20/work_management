e4e648ff5a301e2c002ff42138ff02fbaf1247bb
伊地知 侑一 <ki00215@kyowa.com>
2026-04-28 11:44:24 +0900

    手順追加

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+st.markdown("""
+<style>h1, p{
+text-align: center;}
+</style>""",unsafe_allow_html=True
+)
+
 def main():
-    st.title("Hello, Streamlit!")
-    st.write("This is a simple Streamlit app.")
+    st.title("【DXデモアプリ】")
+    st.write("手順1: 【DXデモアプリ上で打設機の稼働・非稼働時間を計測する】")
+    st.write("手順2: 計測終了後【デモ用アプリ】を.xlsxとしてダウンロードし、【DXデモアプリ】秒単位ずれフォルダにいれる")
+    st.write("手順3: 【実行.batをダブルクリックし、計測終了と次の計測時間の重なりをなくす】")
+    steps = ["実行.batをダブルクリックし、計測終了と次の計測開始時間の重なりをなくす"]
 
 if __name__ == "__main__":
     main()
